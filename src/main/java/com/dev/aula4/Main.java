a3a572840bce6d80cd2c24c459db03a1337dbcad
SobralCybersec <matheussobrallinkedin@gmail.com>
2025-12-17 16:30:19 -0300

    Adicionado Aprofunadamento em POO(OOP) Programação Orientada a Objetos

---
+++ src/main/java/com/dev/aula4/Main.java
@@ -0,0 +1,24 @@
+package com.dev.aula4;
+
+import com.dev.aula4.dto.ResultadoAgendamento;
+import com.dev.aula4.entity.Cliente;
+import com.dev.aula4.repository.AgendaRepository;
+import com.dev.aula4.service.AgendarConsultaService;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Cliente cliente1 = new Cliente("João Silva", "123.456.789-00"); // Objeto / Entidade
+        Cliente cliente2 = new Cliente("Maria Araujinho", "123.456.789-02"); // Objeto / Entidade
+
+        AgendaRepository agendaRepository = new AgendaRepository(); // Banco de Dados
+
+        AgendarConsultaService agendarConsultaService = new AgendarConsultaService(agendaRepository); // Serviço
+
+        ResultadoAgendamento resultado = agendarConsultaService.agendarConsulta(cliente1, LocalDate.now().plusDays(20)); // Executando Agendamento
+        ResultadoAgendamento resultado2 = agendarConsultaService.agendarConsulta(cliente2, LocalDate.now().plusDays(21)); // Executando Agendamento
+    }
+}
---
+++ src/main/java/com/dev/aula4/dto/ResultadoAgendamento.java
@@ -0,0 +1,5 @@
+package com.dev.aula4.dto;
+
+public record ResultadoAgendamento(boolean sucesso, String mensagem) {
+
+}
